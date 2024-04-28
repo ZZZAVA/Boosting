8e8269add605e0fcc7547d378dd29cd8de3f978c
n.zavyalov <work.zava@gmail.com>
2024-04-28 22:12:01 +0300

    final rework of boosting

--- KNN.py
+++ KNN.py
@@ -1,108 +1,122 @@
-import random
-
-import matplotlib.pyplot as plt
 import numpy as np
-from pandas import Series
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import *
-from sklearn.neighbors.base import _check_weights
+from collections import Counter
+import csv
+import random
+import copy
+from sklearn.tree import DecisionTreeClassifier
 
 
-def KNN_Gen(X_veh, y_veh, X_glass, y_glass):
-    acc = 0;
-    test_count = 10
-    dependence_test = Series(index=range(1, 35, 1), dtype=float)
-    # k_res = [ ]
-    for n in range(1, 35, 1):
-        mean_accuracy_test = 0.
-        for _ in range(test_count):
-            X_train, X_test, y_train, y_test = train_test_split(X_veh, y_veh, random_state=random.randint(0, 1000),
-                                                                train_size=0.7)
-            clf = AdaBoostClassifier(base_estimator=KNN(n_neighbors=n), algorithm='SAMME')
-            clf.fit(X_train, np.array(y_train).ravel())
-            mean_accuracy_test += clf.score(X_test, y_test) / test_count
-            dependence_test[n] = mean_accuracy_test
-            acc =mean_accuracy_test
-
-    print("Vehicle err(n):", 1-acc)
-    plt.style.use('seaborn')
-    plt.plot(dependence_test, label='test', marker='.', markersize=1)
-    plt.xlabel('Number of neighbors')
-    plt.ylabel('Accuracy')
-    plt.title('Vehicle')
-    plt.show()
-    dependence_test = Series(index=range(1, 301, 10), dtype=float)
-    # k_res = [ ]
-    for n in range(1, 301, 10):
-        mean_accuracy_test = 0.
-        for _ in range(test_count):
-            X_train, X_test, y_train, y_test = train_test_split(X_veh, y_veh, random_state=random.randint(0, 1000),
-                                                                train_size=0.7)
-            clf = AdaBoostClassifier(base_estimator=KNN(n_neighbors=5), n_estimators=n, algorithm='SAMME')
-            clf.fit(X_train, np.array(y_train).ravel())
-            mean_accuracy_test += clf.score(X_test, y_test) / test_count
-            dependence_test[n] = mean_accuracy_test
-            acc =mean_accuracy_test
-
-    print("Vehicle err(est):", 1-acc)
-    plt.style.use('seaborn')
-    plt.plot(dependence_test, label='test', marker='.', markersize=1)
-    plt.xlabel('Number of estimators')
-    plt.ylabel('Accuracy')
-    plt.title('Vehicle')
-    plt.show()
-    test_count = 10
-
-    dependence_test = Series(index=range(1, 35, 1), dtype=float)
-    for n in range(1, 35, 1):
-        mean_accuracy_test = 0.
-        for _ in range(test_count):
-            X_train, X_test, y_train, y_test = train_test_split(X_glass, y_glass, random_state=random.randint(0, 1000),
-                                                                train_size=0.7)
-            clf = AdaBoostClassifier(base_estimator=KNN(n_neighbors=n), algorithm='SAMME')
-            clf.fit(X_train, np.array(y_train).ravel())
-            mean_accuracy_test += clf.score(X_test, y_test) / test_count
-            dependence_test[n] = mean_accuracy_test
-            acc =mean_accuracy_test
-
-    print("Glass err(n):", 1-acc)
-    plt.style.use('seaborn')
-    plt.plot(dependence_test, label='test', marker='.', markersize=1)
-    plt.xlabel('Number of neighbors')
-    plt.ylabel('Accuracy')
-    plt.title('Glass')
-    plt.show()
+def distance(instance1, instance2):
+    s = 0
+    for i in range(len(instance1) - 1):
+        s += 1 * abs(instance1[i] - instance2[i])
+    return s
 
 
-    for n in range(1, 301, 10):
-        mean_accuracy_test = 0.
-        for _ in range(test_count):
-            X_train, X_test, y_train, y_test = train_test_split(X_glass, y_glass, random_state=random.randint(0, 1000),
-                                                                train_size=0.7)
-            clf = AdaBoostClassifier(base_estimator=KNN(n_neighbors=5), n_estimators=n, algorithm='SAMME')
-            clf.fit(X_train, np.array(y_train).ravel())
-            mean_accuracy_test += clf.score(X_test, y_test) / test_count
-            dependence_test[n] = mean_accuracy_test
-            acc =mean_accuracy_test
+def get_distances(train_main, test_main):
+    train = copy.deepcopy(train_main)
+    test = copy.deepcopy(test_main)
+    for i in range(len(test_main)):
+        test[i].pop(-1)
+    for i in range(len(train_main)):
+        train[i].pop(-1)
+    distances = {}
+    test_ind = 0
+    for i in test:
+        distances[test_ind] = []
+        train_ind = 0
+        for j in train:
+            distances[test_ind].append([train_ind, distance(i, j)])
+            train_ind += 1
+        test_ind += 1
+    return distances
 
-    print("Glass err(est):", 1-acc)
-    plt.style.use('seaborn')
-    plt.plot(dependence_test, label='test', marker='.', markersize=1)
-    plt.xlabel('Number of estimators')
-    plt.ylabel('Accuracy')
-    plt.title('Glass')
-    plt.show()
 
-class KNN(KNeighborsClassifier):
-    def __init__(self, n_neighbors, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski',
-                 metric_params=None, n_jobs=None, **kwargs):
-        super(KNeighborsClassifier, self).__init__(n_neighbors=n_neighbors, algorithm=algorithm, leaf_size=leaf_size,
-                                                   metric=metric, p=p, metric_params=metric_params, n_jobs=n_jobs,
-                                                   **kwargs)
+def get_neighbors(train, train_labels, test_num, k, distances):
+    distances[test_num].sort(key=lambda x: x[1])
+    neighbors = copy.deepcopy(distances[test_num][:k])
+    for i in range(k):
+        neighbors[i].append(train_labels[neighbors[i][0]])
+        neighbors[i].append(train[neighbors[i][0]][-1])
+    return neighbors
 
-        self.weights = _check_weights(weights)
 
-    def fit(self, X, y, sample_weight):
-        return super(KNN, self).fit(X, y)
+def voting(neighbors):
+    class_counter = Counter()
+    for neighbor in neighbors:
+        class_counter[neighbor[2]] += neighbor[3]
+    return class_counter.most_common(1)[0][0]
 
+
+def set_weight(right_label, wrong_label, train, train_labels, neighbors):
+    for i in neighbors:
+        j = i[0]
+        if train_labels[j] == right_label:
+            train[j][-1] *= 2
+        elif train_labels[j] == wrong_label:
+            train[j][-1] /= 2
+
+
+def knn(main_train, main_train_labels):
+    for ix in range(len(main_train)):
+        k = 6
+        train_labels = copy.deepcopy(main_train_labels)
+        test = [main_train[ix]]
+        test_labels = [train_labels[ix]]
+        if ix == 0:
+            train = main_train[1:]
+        elif ix == len(main_train):
+            train = main_train[:ix]
+        else:
+            train = main_train[:ix] + main_train[ix + 1:]
+        d = get_distances(train, test)
+        votes = []
+        for test_ind in range(len(test)):
+            neighbors = get_neighbors(train, train_labels, test_ind, k, d)
+            votes.append(voting(neighbors))
+        errors = {}
+        for i in range(len(votes)):
+            if votes[i] != test_labels[i]:
+                errors[i] = (test_labels[i], votes[i])
+
+        for i in errors:
+            set_weight(errors[i][0], errors[i][1], train, train_labels,
+                       get_neighbors(train, train_labels, i, k + 1, d))
+
+    return main_train
+
+
+def main(dataset, dataset_name):
+    rows = len(dataset)
+    dataset[0].insert(-1, "weight")
+    for i in range(rows):
+        dataset[i].insert(-1, 1)
+    dataset.pop(0)
+    random.shuffle(dataset)
+    train_size = int(0.7 * rows)
+    train_main = dataset[:train_size]
+    test_main = dataset[train_size:]
+    main_train_labels = [row[-1] for row in train_main]
+    main_test_labels = [row[-1] for row in test_main]
+
+    train = knn(train_main, main_train_labels)
+    k = 8
+    test = test_main
+    train_labels = copy.deepcopy(main_train_labels)
+    test_labels = copy.deepcopy(main_test_labels)
+    d = get_distances(train, test)
+    votes = []
+    for test_ind in range(len(test)):
+        neighbors = get_neighbors(train, train_labels, test_ind, k, d)
+        votes.append(voting(neighbors))
+    errors = {}
+    for i in range(len(votes)):
+        if votes[i] != test_labels[i]:
+            errors[i] = test_labels[i]
+
+    print(dataset_name + " knn err:", str(float(len(errors)) / len(test)))
+
+
+def KNN_Gen(filenam_veh, filename_glass):
+    main(filenam_veh, 'vehicle')
+    main(filename_glass, 'glass')
--- ex3.py
+++ ex3.py
@@ -1,15 +1,34 @@
+import csv
+
 import pandas as pd
 
 from Tree import Tree_Gen
 from KNN import KNN_Gen
 
-data = pd.read_csv('Vehicle.csv')
-y_veh = data['Class']
-X_veh = data.loc[:, 'Comp': 'Holl.Ra']
+data_veh = pd.read_csv('Vehicle.csv')
+y_veh = data_veh['Class']
+X_veh = data_veh.loc[:, 'Comp': 'Holl.Ra']
 data_glass = pd.read_csv('glass.csv')
 y_glass = data_glass['Type']
 X_glass = data_glass.loc[:, 'RI': 'Fe']
 
 
+def readfile(filename):
+    data = []
+    with open(filename, "r") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            data.append(row)
+    for row in range(1, len(data)):
+        for el in range(len(data[row]) - 1):
+            data[row][el] = float(data[row][el])
+        try:
+            data[row][-1] = int(float(data[row][-1]))
+        except:
+            pass
+    return data
+
+
 Tree_Gen(X_veh, y_veh, X_glass, y_glass)
-KNN_Gen(X_veh, y_veh, X_glass, y_glass)
+KNN_Gen(readfile('C:/Users/n.zavyalov/PycharmProjects/autodecoder/Vehicle.csv'),
+        readfile('C:/Users/n.zavyalov/PycharmProjects/autodecoder/Glass.csv'))
